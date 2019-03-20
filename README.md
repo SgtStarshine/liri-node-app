39209e8727772e74d4ebafbc51cda1c6bd6e8297
SgtStarshine <starshine621@ymail.com>
2019-03-20 19:26:42 -0400

    updated and working

---
+++ README.md
@@ -0,0 +1,17 @@
+# liri_bot
+LiriBot
+LIRI is a Language Interpretation and Recognition 
+Interface. LIRI will be a command line node app that 
+takes in parameters and gives you back data.
+
+LIRI uses Spotify, OMDB, and Bands In Town API to give 
+the consumer information about a song, a band, or a movie 
+using the following commands. Also feel free to try our 
+command using do-what-it-says and see your result.
+
+spotify-this-song
+concert-this
+movie-this
+
+The return data will be displayed within the command 
+terminal where the commands were entered.
---
+++ liri.js
@@ -0,0 +1,123 @@
+'Liri Bot'
+require("dotenv").config();
+var keys = require('./keys');
+var moment = require('moment');
+var fs = require('fs');
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+var command = process.argv[2];
+if (!process.argv[3] === undefined) {
+    var input = process.argv.slice(3).join(' ');
+}
+runLiRi(command, input);
+function runLiRi(command, input) {
+    switch (command) {
+        case 'concert-this':
+            input === undefined ? runBandsInTown('twenty one pilots') :
+                runBandsInTown(input);
+            break;
+        case 'spotify-this-song':
+            input === undefined ? runSpotify("can't hold us") :
+                runSpotify(input);
+            break;
+        case 'movie-this':
+            input === undefined ? runOMDB('matrix') :
+                runOMDB(input);
+            break;
+        case 'do-what-it-says':
+            runRandom(command, input);
+            break;
+        case 'help':
+            help();
+            break;
+        case undefined:
+        help();
+            break;
+        default:
+            console.log('If your having trouble type in help')
+    };
+};
+function runSpotify(input) {
+    spotify.search({ type: 'track', query: input, limit: 1 }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+        } else {
+            song = data.tracks.items[0];
+            console.log(
+                '\n\nArtist: ' + song.artists[0].name + '\nSong: ' + song.name
+                + '\nPreview Link: ' + song.preview_url + '\nAlbum: ' +
+                song.album.name + '\n\n'
+            );
+        }
+    });
+};
+function runBandsInTown(input) {
+    var request = require('request');
+    request("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp", function (error, response, body) {
+        if (error) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+        }
+        results = JSON.parse(body);
+        if (results.length < 1) {
+            console.log('try some other atrist.\n');
+        } else {
+            console.log('\n Time to plan for the upcoming shows and get some days off from work for ' + input + '\n');
+            for (let i = 0; i < 5 && i < results.length; i++) {
+                console.log('Venue: ' + results[i].venue.name + '\n' +
+                    'Location: ' + results[i].venue.city + ', ' +
+                    results[i].venue.country);
+                var partyDate = results[i].datetime.split('T')[0];
+                partyDate = moment(partyDate, 'YYYY-MM-DD').format('MMM DD, YYYY');
+                if (partyDate) {
+                    console.log('Date: ' + partyDate + '\n');
+                } else {
+                    console.log('Date: Call someone else to find out..... TBA')
+                }
+            }
+        }
+    });
+};
+function runOMDB(input) {
+    var request2 = require('request');
+    request2('http://www.omdbapi.com/?apikey=trilogy&t=' + input, function (error, response, body) {
+        if (error) {
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+        } else {
+            movie = JSON.parse(body);
+            console.log("\n\nTitle: " + movie.Title);
+            console.log("Year: " + movie.Year);
+            console.log("IMDB Rating: " + movie.imdbRating);
+            console.log("Country: " + movie.Country);
+            console.log("Language: " + movie.Language);
+            console.log("Plot: " + movie.Plot);
+            console.log("Actors: " + movie.Actors);
+        }
+    });
+};
+function runRandom(command, input) {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log(error)
+        }
+        data = data.split('"');
+        var dataArr = data;
+        command = dataArr[0].split(',')[0];
+        input = dataArr[1];
+        runLiRi(command, input);
+    });
+};
+function help() {
+    console.log(
+        'In order to run Liri please ask the following\n' +
+        'For concerts around you please use the command\n' +
+        'concert-this "artist", such as concert-this macklemore\n' +
+        'For songs you want info about please use the command\n' +
+        'spotify-this-song "song name here", such as spotify-this-song welcome to the party\n' +
+        'For movies that you want to find more info about please use the command\n' +
+        'movie-this "movie name", such as movie-this Deadpool 2\n' +
+        'For a random command please use\n' +
+        'do-what-it-says\n');
+};
